d2e6f8a907efa030047ff0efb5a28466bdb727ed
Joriiss <78536901+Joriiss@users.noreply.github.com>
2026-04-01 09:32:23 +0200

    géolocalisation ajouté

--- lib/features/map/presentation/map_page.dart
+++ lib/features/map/presentation/map_page.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart' as geo;
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  MapboxMap? _mapboxMap;
+
+  @override
+  void initState() {
+    super.initState();
+    _ensureLocationAndCenter();
+  }
+
+  Future<void> _ensureLocationAndCenter() async {
+    geo.LocationPermission permission = await geo.Geolocator.checkPermission();
+    if (permission == geo.LocationPermission.denied) {
+      permission = await geo.Geolocator.requestPermission();
+    }
+    if (permission == geo.LocationPermission.deniedForever ||
+        permission == geo.LocationPermission.denied) {
+      return;
+    }
+
+    final position = await geo.Geolocator.getCurrentPosition(
+      desiredAccuracy: geo.LocationAccuracy.high,
+    );
+
+    if (_mapboxMap != null) {
+      await _mapboxMap!.setCamera(
+        CameraOptions(
+          center: Point(
+            coordinates: Position(position.longitude, position.latitude),
+          ),
+          zoom: 14,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +55,17 @@ class MapPage extends StatelessWidget {
           ),
           zoom: 12,
         ),
+        onMapCreated: (mapboxMap) async {
+          _mapboxMap = mapboxMap;
+
+          await _mapboxMap!.location.updateSettings(
+            LocationComponentSettings(
+              enabled: true,
+            ),
+          );
+
+          await _ensureLocationAndCenter();
+        },
       ),
     );
   }
